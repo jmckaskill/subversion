ffe2c56d34a78dbfd02ed9dcdb5652108c083ea8
cmpilato <cmpilato@13f79535-47bb-0310-9956-ffa450edef68>
2002-06-12 21:10:35 +0000

    Complete issue #729 with the introduction of the --strict command-line option (though I'm not confident the option is well-named).
    * subversion/include/svn_ra.h
      (svn_ra_plugin_t): Add 'strict_node_history'. 
    
    * subversion/include/svn_repos.h
      (svn_repos_get_logs): Add 'strict_node_history'.
    
    * subversion/clients/cmdline/cl.h
      (svn_cl__longopt_t): Add 'svn_cl__strict_opt' option.
      (svn_cl__opt_state_t): Add 'strict'.
    
    * subversion/libsvn_ra_dav/ra_dav.h
      (svn_ra_dav__get_log): Add 'strict_node_history'.
    
    * subversion/include/svn_client.h
      (svn_client_log): Add 'strict_node_history'.
    
    * subversion/libsvn_fs/tree.c
      (struct revisions_changed_args): Remove 'pool'.
      (svn_fs_revisions_changed): Rework the pool usage here.
      (struct paths_changed_args): New.
      (svn_fs_paths_changed): Begin work on this function.
    
    * subversion/clients/cmdline/main.c
      (svn_cl__cmd_table): Update usage message for `svn log'.
      (svn_cl__options): Handle the --strict option.
      (main): Handle the --strict option.
    
    * subversion/clients/cmdline/log-cmd.c
      (svn_cl__log): Update call to svn_client_log to use new `--strict'
        option status.
    
    * subversion/libsvn_client/log.c
      (svn_client_log): Add 'strict_node_history'.  Update calls to
        ra->get_log to use the new flag.
    
    * subversion/libsvn_ra_local/ra_plugin.c
      (svn_ra_local__get_log): Add 'strict_node_history'.  Update call to
        svn_repos_get_logs to use the new flag.
    
    * subversion/libsvn_ra_dav/log.c
      (svn_ra_dav__get_log): Add 'strict_node_history', and optionally add
        this to the log request. 
    
    * subversion/libsvn_repos/log.c
      (svn_repos_get_logs): Add 'strict_node_history'.  Use it to
        determine how to call svn_fs_revisions_changed.
    
    * subversion/mod_dav_svn/log.c
      (dav_svn__log_report): Parse the 'strict-node-history' tag.  Update
        call to svn_repos_get_logs.
    
    * subversion/tests/clients/cmdline/getopt_tests_data/svn_help_log_switch_stdout
      Update expected stdout results for `svn help log; svn help switch'.

--- subversion/libsvn_ra_dav/log.c
+++ subversion/libsvn_ra_dav/log.c
@@ -246,6 +246,7 @@ svn_error_t * svn_ra_dav__get_log(void *session_baton,
                                   svn_revnum_t start,
                                   svn_revnum_t end,
                                   svn_boolean_t discover_changed_paths,
+                                  svn_boolean_t strict_node_history,
                                   svn_log_message_receiver_t receiver,
                                   void *receiver_baton)
 {
@@ -307,6 +308,13 @@ svn_error_t * svn_ra_dav__get_log(void *session_baton,
       svn_stringbuf_appendcstr(request_body,
                                apr_psprintf(ras->pool,
                                             "<S:discover-changed-paths/>"));
+    }
+
+  if (strict_node_history)
+    {
+      svn_stringbuf_appendcstr(request_body,
+                               apr_psprintf(ras->pool,
+                                            "<S:strict-node-history/>"));
     }
 
   for (i = 0; i < paths->nelts; i++)
