0bcdfbdfd1fc542d5ba4aea73439238551d29721
kfogel <kfogel@13f79535-47bb-0310-9956-ffa450edef68>
2002-11-11 00:46:01 +0000

    First half of issue #943: Add the '--revprop' flag to property operations, thus freeing up '-r' to eventually indicate the revision number for versioned properties.
    * subversion/clients/cmdline/cl.h 
      (svn_cl__revprop_opt): New enum in svn_cl__longopt_t.
      (svn_cl__opt_state_t): Add `revprop' boolean field.
      (svn_cl__revprop_no_rev_error): New prototype.
    
    * subversion/clients/cmdline/main.c 
      (svn_cl__options): Add "--revprop" option.
      (svn_cl__cmd_table): Enable the option for propdel, propedit,
      propget, proplist, and propset.
      (main): Parse the new option.
    
    * subversion/clients/cmdline/props.c 
      (svn_cl__revprop_no_rev_error): New function.
    
    * subversion/clients/cmdline/propget-cmd.c 
      (svn_cl__propget): Distinguish between revprops and versioned
      properties the new way, and as a temporary measure, return an
      unsupported feature error if a revision is specified in the
      versioned property case.
    
    * subversion/clients/cmdline/proplist-cmd.c 
      (svn_cl__proplist): Same.
    
    * subversion/clients/cmdline/propdel-cmd.c 
      (svn_cl__propdel): Distinguish between revprops and versioned
      properties the new way, and return error if a revision is specified
      for the versioned property case, since that doesn't make sense.
    
    * subversion/clients/cmdline/propedit-cmd.c 
      (svn_cl__propedit): Same.
    
    * subversion/clients/cmdline/propset-cmd.c 
      (svn_cl__propset): Same.

--- subversion/clients/cmdline/propdel-cmd.c
+++ subversion/clients/cmdline/propdel-cmd.c
@@ -60,14 +60,18 @@ svn_cl__propdel (apr_getopt_t *os,
   /* Add "." if user passed 0 file arguments */
   svn_opt_push_implicit_dot_target (targets, pool);
 
-  /* Decide if we're deleting a versioned working copy prop or an
-     unversioned repository revision prop.  The existence of the '-r'
-     flag is the key. */
-  if (opt_state->start_revision.kind != svn_opt_revision_unspecified)
+  if (opt_state->revprop)  /* operate on a revprop */
     {
       svn_revnum_t rev;
       const char *URL, *target;
       svn_client_auth_baton_t *auth_baton;
+
+      /* All property commands insist on a specific revision when
+         operating on a revprop. */
+      if (opt_state->start_revision.kind == svn_opt_revision_unspecified)
+        return svn_cl__revprop_no_rev_error (pool);
+
+      /* Else some revision was specified, so proceed. */
 
       auth_baton = svn_cl__make_auth_baton (opt_state, pool);
 
@@ -96,8 +100,14 @@ svn_cl__propdel (apr_getopt_t *os,
                   pname, rev);
         }
     }
-
-  else
+  else if (opt_state->start_revision.kind != svn_opt_revision_unspecified)
+    {
+      return svn_error_createf
+        (SVN_ERR_CL_ARG_PARSING_ERROR, 0, NULL,
+         "Cannot specify revision for deleting versioned property '%s'.",
+         pname);
+    }
+  else  /* operate on a normal, versioned property (not a revprop) */
     {
       /* For each target, remove the property PNAME. */
       for (i = 0; i < targets->nelts; i++)
