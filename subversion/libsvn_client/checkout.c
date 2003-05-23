822c1ac81682dfe9b3d90531c76ffbde257391c3
sussman <sussman@13f79535-47bb-0310-9956-ffa450edef68>
2003-05-23 21:55:58 +0000

    A new checkout implementation:  don't call RA->do_checkout(); instead have svn_client_checkout() secretly run svn_client_update() on an incomplete wc-root dir.
    [In my latest tests, this change makes ra_local and ra_dav checkouts
    about 10% faster, and ra_svn checkouts are the same speed.]
    
    * cmdline/checkout-cmd.c (svn_cl__checkout): don't ever pass an
      'unspecified' revision into svn_client_checkout, because
      svn_client_checkout claims it won't accept such; default to HEAD
      instead.
    
    * svn_wc.h (svn_wc_ensure_adm):  declare as a public function.
    
    * libsvn_wc/adm_files.h (svn_wc__ensure_adm):  remove declaration.
    
    * libsvn_wc/adm_files.c (svn_wc_ensure_adm):  was svn_wc__ensure_adm.
    
    * libsvn_wc/adm_ops.c (svn_wc_add):  update caller.
    
    * libsvn_wc/update_editor.c (prep directory): update caller.
    
    * libsvn_client/checkout.c (svn_client__checkout_internal): fulfill
      docstring promise; opt_revision_t argument mustn't be 'unspecified'.
      Also, instead of calling RA->checkout(), create a new incomplete
      root wc-dir and run svn_client_update on it.
    
    * svnadmin_tests.py (create_txn, remove_txn):  increment all expected
      txn numbers by 1.   my forthcoming mail about this.  :-)

--- subversion/libsvn_client/checkout.c
+++ subversion/libsvn_client/checkout.c
@@ -63,6 +63,13 @@ svn_client__checkout_internal (const char *URL,
   assert (path != NULL);
   assert (URL != NULL);
 
+  /* Fulfill the docstring promise of svn_client_checkout: */
+  if ((revision->kind != svn_opt_revision_number)
+      && (revision->kind != svn_opt_revision_date)
+      && (revision->kind != svn_opt_revision_head))
+    return svn_error_create (SVN_ERR_CLIENT_BAD_REVISION, NULL,
+                             "Bogus revision passed to svn_client_checkout");
+
   /* Get revnum set to something meaningful, so we can fetch the
      checkout editor. */
   if (revision->kind == svn_opt_revision_number)
@@ -107,14 +114,14 @@ svn_client__checkout_internal (const char *URL,
       SVN_ERR (svn_client__get_revision_number
                (&revnum, ra_lib, session, revision, path, pool));
 
-      /* Tell RA to do a checkout of REVISION; if we pass an invalid
-         revnum, that means RA will fetch the latest revision.  */
-      err = ra_lib->do_checkout (session,
-                                 revnum,
-                                 recurse,
-                                 checkout_editor,
-                                 checkout_edit_baton,
-                                 pool);
+      /* Bootstrap: create an incomplete working-copy root dir.  Its
+         entries file should only have an entry for THIS_DIR with a
+         URL, revnum, and an 'incomplete' flag.  */
+      SVN_ERR (svn_io_make_dir_recursively (path, pool));
+      SVN_ERR (svn_wc_ensure_adm (path, URL, revnum, pool));
+
+      /* Have update flesh everything out. */
+      err = svn_client_update (path, revision, recurse, ctx, pool);
 
       if (err)
         {
