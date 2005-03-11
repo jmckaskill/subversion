38033dccf911662249c78ff6c440f0bb3cd86616
cmpilato <cmpilato@13f79535-47bb-0310-9956-ffa450edef68>
2005-03-11 21:49:22 +0000

    More bugfixes for repository diffs.
    * subversion/libsvn_client/repos_diff.c 
      (open_root): Fetch dirprops for the root directory unconditionally.
        Besides, target paths are never NULL (though they may be empty).
    
    * subversion/libsvn_client/diff.c
      (display_prop_diffs): Don't show no-op diffs.  This can happen if a
        new client is hitting a mod_dav_svn server that doesn't understand
        the new(ish) "send-all" REPORT style.

--- subversion/libsvn_client/diff.c
+++ subversion/libsvn_client/diff.c
@@ -113,6 +113,13 @@ display_prop_diffs (const apr_array_header_t *propchanges,
       else
         original_value = NULL;
 
+      /* If the property doesn't exist on either side, or if it exists
+         with the same value, skip it.  */
+      if ((! (original_value || propchange->value))
+          || (original_value && propchange->value
+              && svn_string_compare (original_value, propchange->value)))
+        continue;
+
       SVN_ERR (file_printf_from_utf8 (file, _("Name: %s%s"),
                                       propchange->name, APR_EOL_STR));
 
