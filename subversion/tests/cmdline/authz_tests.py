30273825c4d17f15ca13c2aba1687f842d78496e
lundblad <lundblad@13f79535-47bb-0310-9956-ffa450edef68>
2006-05-16 10:14:59 +0000

    Authz test for locking: try to lock a file that's readonly. This test is also a test for issue 2548, which was fixed in r19620.
    Patch by: Lieven Govaerts <lgo@mobsol.be>
    
    * subversion/tests/cmdline/authz_tests.py
      (authz_write_access): Extend to try to lock a read-only file.

--- subversion/tests/cmdline/authz_tests.py
+++ subversion/tests/cmdline/authz_tests.py
@@ -437,6 +437,18 @@ def authz_write_access(sbox):
                                      '-m', 'logmsg',
                                      B_url, D_url)
 
+  if sbox.repo_url.startswith('svn'):
+    expected_err = ".*svn: Authorization failed.*"
+
+  # lock a file, target is readonly: should fail
+  svntest.actions.run_and_verify_svn("",
+                                     None, expected_err,
+                                     'lock',
+                                     '--username', svntest.main.wc_author,
+                                     '--password', svntest.main.wc_passwd,
+                                     '-m', 'lock msg',
+                                     iota_url)
+
 #----------------------------------------------------------------------
 
 def authz_checkout_test(sbox):
