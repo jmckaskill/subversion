3adcc5192a83103665eb243408ac6690c8d7c89c
pburba <pburba@13f79535-47bb-0310-9956-ffa450edef68>
2006-08-08 16:24:59 +0000

    Merge r20190, 20250, 20444, and 20826 from trunk to branches/1.4.x.
    A few improvements to the Windows build system.
    
    Votes: +1: djh, rooneg, brane (r20826 only from email), pburba
           +0: dlr

--- win-tests.py
+++ win-tests.py
@@ -95,6 +95,10 @@ else:
   abs_builddir = os.path.abspath(args[0])
   create_dirs = 1
 
+# Don't run bdb tests if they want to test fsfs
+if fs_type == 'fsfs':
+  all_tests = gen_obj.test_progs + gen_obj.scripts
+
 if run_httpd:
   if not httpd_port:
     httpd_port = random.randrange(1024, 30000)
