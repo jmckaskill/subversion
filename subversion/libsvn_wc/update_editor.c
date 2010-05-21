jmckaskill/subversion

commit 1e5fc92a73296d9aeaa1a80b4a4a14d8ee0264d6

* subversion/libsvn_wc/update_editor.c   (absent_file_or_dir): Remove use of an svn_wc_entry_t.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@947041 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/update_editor.c
@@ -3226,32 +3226,21 @@ absent_file_or_dir(const char *path,
   const char *repos_relpath;
   const char *repos_root_url;
   const char *repos_uuid;
+  svn_boolean_t is_added;
   svn_wc__db_kind_t db_kind
     = kind == svn_node_dir ? svn_wc__db_kind_dir : svn_wc__db_kind_file;
 
   local_abspath = svn_dirent_join(pb->local_abspath, name, pool);
 
-  /* Extra check: an item by this name may not exist, but there may
-     still be one scheduled for addition.  That's a genuine
-     tree-conflict.  */
-  {
-    svn_boolean_t in_parent = (kind == svn_node_dir);
-    const svn_wc_entry_t *entry;
-    svn_boolean_t hidden;
-    SVN_ERR(svn_wc__get_entry(&entry, eb->db, local_abspath, TRUE, kind,
-                              in_parent, pool, pool));
-
-    if (entry)
-      SVN_ERR(svn_wc__entry_is_hidden(&hidden, entry));
-
-    /* ### BH: With WC-NG we should probably also check for replaced? */
-    if (entry && !hidden && (entry->schedule == svn_wc_schedule_add))
-      return svn_error_createf(
+  /* If an item by this name is scheduled for addition that's a
+     genuine tree-conflict.  */
+  SVN_ERR(svn_wc__node_is_added(&is_added, eb->wc_ctx, local_abspath, pool));
+  if (is_added)
+    return svn_error_createf(
          SVN_ERR_WC_OBSTRUCTED_UPDATE, NULL,
          _("Failed to mark '%s' absent: item of the same name is already "
            "scheduled for addition"),
          svn_dirent_local_style(path, pool));
-  }
 
   SVN_ERR(svn_wc__db_scan_base_repos(&repos_relpath, &repos_root_url,
                                      &repos_uuid, eb->db, pb->local_abspath,

commit 0b0133d1e77134794169f88a5eae650684af3caa

Fix regression tests failures introduced by r947041.
* subversion/libsvn_wc/update_editor.c
  (absent_file_or_dir): Only check if versioned nodes are added.

subversion/libsvn_wc/update_editor.c
@@ -3227,6 +3227,7 @@ absent_file_or_dir(const char *path,
   const char *repos_root_url;
   const char *repos_uuid;
   svn_boolean_t is_added;
+  svn_node_kind_t existing_kind;
   svn_wc__db_kind_t db_kind
     = kind == svn_node_dir ? svn_wc__db_kind_dir : svn_wc__db_kind_file;
 
@@ -3234,13 +3235,19 @@ absent_file_or_dir(const char *path,
 
   /* If an item by this name is scheduled for addition that's a
      genuine tree-conflict.  */
-  SVN_ERR(svn_wc__node_is_added(&is_added, eb->wc_ctx, local_abspath, pool));
-  if (is_added)
-    return svn_error_createf(
+  SVN_ERR(svn_wc_read_kind(&existing_kind, eb->wc_ctx, local_abspath, TRUE,
+                           pool));
+  if (existing_kind != svn_node_none)
+    {
+      SVN_ERR(svn_wc__node_is_added(&is_added, eb->wc_ctx, local_abspath,
+                                    pool));
+      if (is_added)
+        return svn_error_createf(
          SVN_ERR_WC_OBSTRUCTED_UPDATE, NULL,
          _("Failed to mark '%s' absent: item of the same name is already "
            "scheduled for addition"),
          svn_dirent_local_style(path, pool));
+    }
 
   SVN_ERR(svn_wc__db_scan_base_repos(&repos_relpath, &repos_root_url,
                                      &repos_uuid, eb->db, pb->local_abspath,