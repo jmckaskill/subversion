jmckaskill/subversion

commit 748a95f8313d25cce5f3e1608f49b043ce089239

As a first step towards eliminating use of the node walker in svn_wc_revision_status2(), use a DB query to determine the min/max revisions of the working copy, instead of obtaining this information during the crawl.
* subversion/libsvn_wc/revision_status.c
  (analyze_status): Ignore revision number information during the crawl.
  (svn_wc_revision_status2): Get revision number information from wc.db
   using the new svn_wc__db_get_min_max_revisions() function.

* subversion/libsvn_wc/wc-queries.sql
  (STMT_SELECT_MIN_MAX_REVISIONS): New query which returns revision
   information previously obtained during the crawl in analyze_status().

* subversion/libsvn_wc/wc_db.c
  (svn_wc__db_get_min_max_revisions): New. Runs the above query.

* subversion/libsvn_wc/wc_db.h
  (svn_wc__db_get_min_max_revisions): Declare.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1076712 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/revision_status.c
@@ -104,19 +104,6 @@ analyze_status(const char *local_abspath,
               ? changed_rev
               : revision);
 
-  /* Added files have a revision of no interest */
-  if (item_rev != SVN_INVALID_REVNUM)
-    {
-
-      if (wb->result->min_rev == SVN_INVALID_REVNUM
-          || item_rev < wb->result->min_rev)
-        wb->result->min_rev = item_rev;
-
-      if (wb->result->max_rev == SVN_INVALID_REVNUM
-          || item_rev > wb->result->max_rev)
-        wb->result->max_rev = item_rev;
-    }
-
   if (! wb->result->modified)
     {
       svn_boolean_t props_mod;
@@ -195,6 +182,9 @@ svn_wc_revision_status2(svn_wc_revision_status_t **result_p,
         }
     }
 
+  SVN_ERR(svn_wc__db_get_min_max_revisions(&result->min_rev, &result->max_rev,
+                                           wc_ctx->db, local_abspath,
+                                           scratch_pool));
   SVN_ERR(svn_wc__node_walk_children(wc_ctx,
                                      local_abspath,
                                      TRUE /* show_hidden */,

subversion/libsvn_wc/wc-queries.sql
@@ -841,6 +841,13 @@ ORDER BY local_relpath;
 
 /* ------------------------------------------------------------------------- */
 
+-- STMT_SELECT_MIN_MAX_REVISIONS
+SELECT MIN(revision), MAX(revision) FROM nodes
+  WHERE wc_id = ?1
+  AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
+  AND (presence = 'normal' OR presence = 'incomplete')
+  AND op_depth = 0;
+
 /* Grab all the statements related to the schema.  */
 
 -- include: wc-metadata

subversion/libsvn_wc/wc_db.c
@@ -9147,3 +9147,48 @@ svn_wc__db_temp_below_work(svn_boolean_t *have_work,
 
   return SVN_NO_ERROR;
 }
+
+svn_error_t *
+svn_wc__db_get_min_max_revisions(svn_revnum_t *min_revision,
+                                 svn_revnum_t *max_revision,
+                                 svn_wc__db_t *db,
+                                 const char *local_abspath,
+                                 apr_pool_t *scratch_pool)
+{
+  svn_wc__db_wcroot_t *wcroot;
+  const char *local_relpath;
+  svn_sqlite__stmt_t *stmt;
+  svn_boolean_t have_row;
+
+  SVN_ERR_ASSERT(svn_dirent_is_absolute(local_abspath));
+
+  SVN_ERR(svn_wc__db_wcroot_parse_local_abspath(&wcroot, &local_relpath,
+                                                db, local_abspath,
+                                                svn_sqlite__mode_readwrite,
+                                                scratch_pool, scratch_pool));
+  VERIFY_USABLE_WCROOT(wcroot);
+
+  SVN_ERR(svn_sqlite__get_statement(&stmt, wcroot->sdb,
+                                    STMT_SELECT_MIN_MAX_REVISIONS));
+  SVN_ERR(svn_sqlite__bindf(stmt, "iss", wcroot->wc_id, local_relpath,
+                            construct_like_arg(local_relpath, scratch_pool)));
+  SVN_ERR(svn_sqlite__step(&have_row, stmt));
+  if (have_row)
+    {
+        *min_revision = svn_sqlite__column_revnum(stmt, 0);
+        *max_revision = svn_sqlite__column_revnum(stmt, 1);
+    }
+  else
+    {
+        *min_revision = SVN_INVALID_REVNUM;
+        *max_revision = SVN_INVALID_REVNUM;
+    }
+
+  /* The statement should only return at most one row. */
+  SVN_ERR(svn_sqlite__step(&have_row, stmt));
+  SVN_ERR_ASSERT(! have_row);
+
+  SVN_ERR(svn_sqlite__reset(stmt));
+
+  return SVN_NO_ERROR;
+}

subversion/libsvn_wc/wc_db.h
@@ -2500,6 +2500,19 @@ svn_wc__db_temp_below_work(svn_boolean_t *have_work,
                            apr_pool_t *scratch_pool);
 
 
+/* Set *MIN_REVISION and *MAX_REVISION to the lowest and highest revision
+ * numbers found within LOCAL_ABSPATH in the working copy using DB.
+ * Only nodes with op_depth zero and presence 'normal' or 'incomplete'
+ * are considered, so that added, deleted or excluded nodes do not affect
+ * the result. */
+svn_error_t *
+svn_wc__db_get_min_max_revisions(svn_revnum_t *min_revision,
+                                 svn_revnum_t *max_revision,
+                                 svn_wc__db_t *db,
+                                 const char *local_abspath,
+                                 apr_pool_t *scratch_pool);
+
+
 /* @} */
 
 

commit 091218b82ba1f95847d7eb54474e4fc35ce7a199

Unbreak "svnversion --committed" (broken by r1076712). Also add a simple test for it so that mistakes like this don't happen again.
* subversion/tests/cmdline/svntest/actions.py
  (run_and_verify_svnversion2, run_and_verify_svnversion): Add VARARGS
   parameter, so that svnversion tests can pass arbitrary command line
   options to svnversion.

* subversion/tests/cmdline/svnversion_tests.py
  (committed_revisions, test_list): New test.

* subversion/libsvn_wc/revision_status.c
  (walk_baton): Remove COMMITTED member.
  (analyze_status): The change made in r1076712 was incomplete. It left
   some code in place used in the --committed case, but in a non-working
   state. Now that the new svn_wc__db_revision_status() interface can
   handle this, stop caring about any kind of revision information.
  (svn_wc_revision_status2): Pass COMMITTED to svn_wc_revision_status2(),
   and track its removal from the walk_baton.

* subversion/libsvn_wc/wc-queries.sql
  (STMT_SELECT_MIN_MAX_REVISIONS): Return min/max committed (i.e. "last
   changed") revisions, too.

* subversion/libsvn_wc/wc_db.c,
  subversion/libsvn_wc/wc_db.h
  (svn_wc__db_revision_status): Add COMMITTED parameter which sets
   *MIN_REVISION and *MAX_REVISION to "last changed" min/max revisions.

subversion/libsvn_wc/revision_status.c
@@ -35,7 +35,6 @@
 struct walk_baton
 {
   svn_wc_revision_status_t *result;           /* where to put the result */
-  svn_boolean_t committed;           /* examine last committed revisions */
   const char *local_abspath;         /* path whose URL we're looking for */
   svn_wc__db_t *db;
 };
@@ -57,14 +56,11 @@ analyze_status(const char *local_abspath,
                apr_pool_t *scratch_pool)
 {
   struct walk_baton *wb = baton;
-  svn_revnum_t changed_rev;
-  svn_revnum_t revision;
   svn_revnum_t item_rev;
   svn_boolean_t is_file_external;
   svn_wc__db_status_t status;
 
-  SVN_ERR(svn_wc__db_read_info(&status, NULL, &revision, NULL,
-                               NULL, NULL, &changed_rev,
+  SVN_ERR(svn_wc__db_read_info(&status, NULL, NULL, NULL, NULL, NULL, NULL,
                                NULL, NULL, NULL, NULL, NULL, NULL, NULL,
                                NULL, NULL, NULL, NULL, NULL, NULL,
                                NULL, NULL, NULL, NULL, wb->db,
@@ -80,10 +76,6 @@ analyze_status(const char *local_abspath,
                                             local_abspath, scratch_pool));
   if (is_file_external)
     return SVN_NO_ERROR;
-
-  item_rev = (wb->committed
-              ? changed_rev
-              : revision);
 
   if (! wb->result->modified)
     {
@@ -138,8 +130,8 @@ svn_wc_revision_status2(svn_wc_revision_status_t **result_p,
                                      &result->sparse_checkout,
                                      &result->modified,
                                      &result->switched,
-                                     wc_ctx->db,
-                                     local_abspath, scratch_pool));
+                                     wc_ctx->db, local_abspath,
+                                     committed, scratch_pool));
 
   if (!result->switched && trail_url != NULL)
     {
@@ -165,7 +157,6 @@ svn_wc_revision_status2(svn_wc_revision_status_t **result_p,
 
   /* initialize walking baton */
   wb.result = result;
-  wb.committed = committed;
   wb.local_abspath = local_abspath;
   wb.db = wc_ctx->db;
 

subversion/libsvn_wc/wc-queries.sql
@@ -870,7 +870,8 @@ ORDER BY local_relpath;
 /* Queries for revision status. */
 
 -- STMT_SELECT_MIN_MAX_REVISIONS
-SELECT MIN(revision), MAX(revision) FROM nodes
+SELECT MIN(revision), MAX(revision),
+       MIN(changed_revision), MAX(changed_revision) FROM nodes
   WHERE wc_id = ?1
   AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
   AND (presence = 'normal' OR presence = 'incomplete')

subversion/libsvn_wc/wc_db.c
@@ -9136,6 +9136,7 @@ svn_wc__db_revision_status(svn_revnum_t *min_revision,
                            svn_boolean_t *is_switched,
                            svn_wc__db_t *db,
                            const char *local_abspath,
+                           svn_boolean_t committed,
                            apr_pool_t *scratch_pool)
 {
   svn_sqlite__stmt_t *stmt;
@@ -9160,8 +9161,16 @@ svn_wc__db_revision_status(svn_revnum_t *min_revision,
   SVN_ERR(svn_sqlite__step(&have_row, stmt));
   if (have_row)
     {
-        *min_revision = svn_sqlite__column_revnum(stmt, 0);
-        *max_revision = svn_sqlite__column_revnum(stmt, 1);
+        if (committed)
+          {
+            *min_revision = svn_sqlite__column_revnum(stmt, 2);
+            *max_revision = svn_sqlite__column_revnum(stmt, 3);
+          }
+        else
+          {
+            *min_revision = svn_sqlite__column_revnum(stmt, 0);
+            *max_revision = svn_sqlite__column_revnum(stmt, 1);
+          }
     }
   else
     {

subversion/libsvn_wc/wc_db.h
@@ -2507,7 +2507,9 @@ svn_wc__db_temp_below_work(svn_boolean_t *have_work,
  * numbers found within LOCAL_ABSPATH.
  * Only nodes with op_depth zero and presence 'normal' or 'incomplete'
  * are considered, so that added, deleted or excluded nodes do not affect
- * the result.
+ * the result.  If COMMITTED is TRUE, set *MIN_REVISION and *MAX_REVISION
+ * to the lowest and highest comitted (i.e. "last changed") revision numbers,
+ * respectively.
  *
  * Indicate in *IS_SPARSE_CHECKOUT whether any of the nodes within
  * LOCAL_ABSPATH is sparse.
@@ -2522,6 +2524,7 @@ svn_wc__db_revision_status(svn_revnum_t *min_revision,
                            svn_boolean_t *is_switched,
                            svn_wc__db_t *db,
                            const char *local_abspath,
+                           svn_boolean_t committed,
                            apr_pool_t *scratch_pool);
 
 /* @} */

subversion/tests/cmdline/svntest/actions.py
@@ -227,7 +227,7 @@ def run_and_verify_svnadmin2(message, expected_stdout, expected_stderr,
 
 
 def run_and_verify_svnversion(message, wc_dir, repo_url,
-                              expected_stdout, expected_stderr):
+                              expected_stdout, expected_stderr, *varargs):
   """like run_and_verify_svnversion2, but the expected exit code is
   assumed to be 0 if no output is expected on stderr, and 1 otherwise."""
 
@@ -236,14 +236,14 @@ def run_and_verify_svnversion(message, wc_dir, repo_url,
     expected_exit = 1
   return run_and_verify_svnversion2(message, wc_dir, repo_url,
                                     expected_stdout, expected_stderr,
-                                    expected_exit)
+                                    expected_exit, *varargs)
 
 def run_and_verify_svnversion2(message, wc_dir, repo_url,
                                expected_stdout, expected_stderr,
-                               expected_exit):
+                               expected_exit, *varargs):
   """Run svnversion command and check its output and exit code."""
 
-  exit_code, out, err = main.run_svnversion(wc_dir, repo_url)
+  exit_code, out, err = main.run_svnversion(wc_dir, repo_url, *varargs)
   verify.verify_outputs("Unexpected output", out, err,
                         expected_stdout, expected_stderr)
   verify.verify_exit_code(message, exit_code, expected_exit)

subversion/tests/cmdline/svnversion_tests.py
@@ -292,6 +292,20 @@ def svnversion_with_structural_changes(sbox):
   svntest.actions.run_and_verify_svnversion("Deleted file", wc_dir, repo_url,
                                             [ "1:2M\n" ], [])
 
+def committed_revisions(sbox):
+  "test 'svnversion --committed'"
+  sbox.build()
+  wc_dir = sbox.wc_dir
+  repo_url = sbox.repo_url
+
+  sbox.simple_copy('iota', 'iota2')
+  sbox.simple_commit()
+  sbox.simple_update()
+  svntest.actions.run_and_verify_svnversion("Committed revisions", wc_dir, repo_url,
+                                            [ "1:2\n" ], [],
+                                            "--committed")
+
+
 ########################################################################
 # Run the tests
 
@@ -302,6 +316,7 @@ test_list = [ None,
               ignore_externals,
               svnversion_with_excluded_subtrees,
               svnversion_with_structural_changes,
+              committed_revisions,
              ]
 
 if __name__ == '__main__':