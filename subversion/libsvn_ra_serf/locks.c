jmckaskill/subversion

commit 2e1e73599ac2fae2fdf9cc4ca80ed609bbf666f1

In ra_serf: Never return APR_EGENERAL to callers as thas makes it impossible for applications to even guess what went wrong, and hard for us to fix the problems that cause this error
* subversion/libsvn_ra_serf/merge.c
  (end_merge): Use the generic request failed error.

* subversion/libsvn_ra_serf/serf.c
  (svn_ra_serf__get_uuid): Return a real error code.

* subversion/libsvn_ra_serf/util.c
  (start_error): Use SVN_ERR_RA_DAV_REQUEST_FAILED as default error code
    instead of the generic apr error.
  (handle_response): Don't try to parse the text of a http 405 error as
    xml and provide a slightly better error than "Unspecified error message"
    which at least tells us which request failed.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1154982 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_ra_serf/merge.c
@@ -294,8 +294,7 @@ end_merge(svn_ra_serf__xml_parser_t *parser,
           href = apr_hash_get(info->props, "href", APR_HASH_KEY_STRING);
           if (! svn_urlpath__is_ancestor(ctx->merge_url, href))
             {
-              /* ### need something better than APR_EGENERAL */
-              return svn_error_createf(APR_EGENERAL, NULL,
+              return svn_error_createf(SVN_ERR_RA_DAV_REQUEST_FAILED, NULL,
                                        _("A MERGE response for '%s' is not "
                                          "a child of the destination ('%s')"),
                                        href, ctx->merge_url);

subversion/libsvn_ra_serf/serf.c
@@ -1110,7 +1110,7 @@ svn_ra_serf__get_uuid(svn_ra_session_t *ra_session,
       SVN_ERR(svn_ra_serf__discover_vcc(&vcc_url, session, NULL, pool));
       if (!session->uuid)
         {
-          return svn_error_create(APR_EGENERAL, NULL,
+          return svn_error_create(SVN_ERR_RA_DAV_RESPONSE_HEADER_BADNESS, NULL,
                                   _("The UUID property was not found on the "
                                     "resource or any of its parents"));
         }

subversion/libsvn_ra_serf/util.c
@@ -821,7 +821,7 @@ start_error(svn_ra_serf__xml_parser_t *parser,
         }
       else
         {
-          ctx->error->apr_err = APR_EGENERAL;
+          ctx->error->apr_err = SVN_ERR_RA_DAV_REQUEST_FAILED;
         }
 
       /* Start collecting cdata. */
@@ -2013,17 +2013,19 @@ handle_response(serf_request_t *request,
 
   ctx->conn->last_status_code = sl.code;
 
-  if (sl.code == 409 || sl.code >= 500)
+  if (sl.code == 405 || sl.code == 409 || sl.code >= 500)
     {
-      /* 409 Conflict: can indicate a hook error.
+      /* 405 Method Not allowed.
+         409 Conflict: can indicate a hook error.
          5xx (Internal) Server error. */
       SVN_ERR(svn_ra_serf__handle_server_error(request, response, pool));
 
       if (!ctx->session->pending_error)
         {
           return
-              svn_error_createf(APR_EGENERAL, NULL,
-              _("Unspecified error message: %d %s"), sl.code, sl.reason);
+              svn_error_createf(SVN_ERR_RA_DAV_REQUEST_FAILED, NULL,
+                                _("The %s request on '%s' failed: %d %s"),
+                                ctx->method, ctx->path, sl.code, sl.reason);
         }
 
       return SVN_NO_ERROR; /* Error is set in caller */

commit 05b660fe8ceb75b6ed1109662034fd7fb7a6c23e

Merge r1154982, r1155124 from trunk:
 * r1154982, r1155124
   Stop returning APR_EGENERAL from serf for unexpected data.
   Justification:
     Clients like AnkhSVN can't see what kind of error this is so they can
     only show a dialog with an error which in some cases is even not clear
     to normal users.
   Notes:
     r1154982 improves the generic errors and r1155124 restores some
     specific 'svn lock' behavior that was broken by r1154982.
   Votes:
     +1: rhuijben, stsp, cmpilato

STATUS
@@ -148,18 +148,6 @@ Veto-blocked changes:
 Approved changes:
 =================
 
- * r1154982, r1155124
-   Stop returning APR_EGENERAL from serf for unexpected data.
-   Justification:
-     Clients like AnkhSVN can't see what kind of error this is so they can
-     only show a dialog with an error which in some cases is even not clear
-     to normal users.
-   Notes:
-     r1154982 improves the generic errors and r1155124 restores some
-     specific 'svn lock' behavior that was broken by r1154982.
-   Votes:
-     +1: rhuijben, stsp, cmpilato
-
  * r1155131
    Cleanup the xml parser in serf in unexpected conditions.
    Justification:

subversion/libsvn_ra_serf/locks.c
@@ -385,14 +385,6 @@ handle_lock(serf_request_t *request,
           return err;
         }
 
-      /* 405 == Method Not Allowed (Occurs when trying to lock a working
-         copy path which no longer exists at HEAD in the repository. */
-      if (sl.code == 405)
-        return svn_error_createf(SVN_ERR_FS_OUT_OF_DATE,
-                                 NULL,
-                                 _("Lock request failed: %d %s"),
-                                   ctx->status_code, ctx->reason);
-
       headers = serf_bucket_response_get_headers(response);
 
       val = serf_bucket_headers_get(headers, SVN_DAV_LOCK_OWNER_HEADER);

subversion/libsvn_ra_serf/merge.c
@@ -294,8 +294,7 @@ end_merge(svn_ra_serf__xml_parser_t *parser,
           href = apr_hash_get(info->props, "href", APR_HASH_KEY_STRING);
           if (! svn_urlpath__is_ancestor(ctx->merge_url, href))
             {
-              /* ### need something better than APR_EGENERAL */
-              return svn_error_createf(APR_EGENERAL, NULL,
+              return svn_error_createf(SVN_ERR_RA_DAV_REQUEST_FAILED, NULL,
                                        _("A MERGE response for '%s' is not "
                                          "a child of the destination ('%s')"),
                                        href, ctx->merge_url);

subversion/libsvn_ra_serf/serf.c
@@ -1028,7 +1028,7 @@ svn_ra_serf__get_uuid(svn_ra_session_t *ra_session,
       SVN_ERR(svn_ra_serf__discover_vcc(&vcc_url, session, NULL, pool));
       if (!session->uuid)
         {
-          return svn_error_create(APR_EGENERAL, NULL,
+          return svn_error_create(SVN_ERR_RA_DAV_RESPONSE_HEADER_BADNESS, NULL,
                                   _("The UUID property was not found on the "
                                     "resource or any of its parents"));
         }

subversion/libsvn_ra_serf/util.c
@@ -757,7 +757,7 @@ start_error(svn_ra_serf__xml_parser_t *parser,
         }
       else
         {
-          ctx->error->apr_err = APR_EGENERAL;
+          ctx->error->apr_err = SVN_ERR_RA_DAV_REQUEST_FAILED;
         }
 
       /* Start collecting cdata. */
@@ -1803,17 +1803,27 @@ handle_response(serf_request_t *request,
 
   ctx->conn->last_status_code = sl.code;
 
-  if (sl.code == 409 || sl.code >= 500)
+  if (sl.code == 405 || sl.code == 409 || sl.code >= 500)
     {
-      /* 409 Conflict: can indicate a hook error.
+      /* 405 Method Not allowed.
+         409 Conflict: can indicate a hook error.
          5xx (Internal) Server error. */
       SVN_ERR(svn_ra_serf__handle_server_error(request, response, pool));
 
       if (!ctx->session->pending_error)
         {
+          apr_status_t apr_err = SVN_ERR_RA_DAV_REQUEST_FAILED;
+
+          /* 405 == Method Not Allowed (Occurs when trying to lock a working
+             copy path which no longer exists at HEAD in the repository. */
+
+          if (sl.code == 405 && !strcmp(ctx->method, "LOCK"))
+            apr_err = SVN_ERR_FS_OUT_OF_DATE;
+
           return
-              svn_error_createf(APR_EGENERAL, NULL,
-              _("Unspecified error message: %d %s"), sl.code, sl.reason);
+              svn_error_createf(apr_err, NULL,
+                                _("%s request on '%s' failed: %d %s"),
+                                ctx->method, ctx->path, sl.code, sl.reason);
         }
 
       return SVN_NO_ERROR; /* Error is set in caller */

subversion/tests/cmdline/lock_tests.py
@@ -1499,9 +1499,9 @@ def lock_path_not_in_head(sbox):
   svntest.actions.run_and_verify_svn(None, None, [], 'commit',
                                      '-m', 'Some deletions', wc_dir)
   svntest.actions.run_and_verify_svn(None, None, [], 'up', '-r1', wc_dir)
-  expected_lock_fail_err_re = "svn:.*" \
+  expected_lock_fail_err_re = "svn: warning: W160042: " \
   "((Path .* doesn't exist in HEAD revision)" \
-  "|(Lock request failed: 405 Method Not Allowed))"
+  "|(L(ock|OCK) request (on '.*' )?failed: 405 Method Not Allowed))"
   # Issue #3524 These lock attemtps were triggering an assert over ra_serf:
   #
   # working_copies\lock_tests-37>svn lock A\D