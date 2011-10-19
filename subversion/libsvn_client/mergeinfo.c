3fd40f2409d7ae0cd0a03f92e56effd2684a5fce
julianfoad <julianfoad@13f79535-47bb-0310-9956-ffa450edef68>
2011-10-19 10:08:23 +0000

    Don't force callers of svn_client__ra_session_from_path() to receive unwanted outputs: make the 'rev_p' and 'url_p' parameters optional.
    * subversion/libsvn_client/client.h
      (svn_client__ra_session_from_path): Document 'rev_p' and 'url_p' as
        optional.
    
    * subversion/libsvn_client/blame.c
      (svn_client_blame5): Don't receive an unwanted URL.
    
    * subversion/libsvn_client/log.c
      (svn_client_log5): Don't receive an unwanted revnum.
    
    * subversion/libsvn_client/mergeinfo.c
      (svn_client_mergeinfo_log): Don't receive an unwanted URL.
    
    * subversion/libsvn_client/ra.c
      (resolve_rev_and_url, svn_client__ra_session_from_path): Make 'rev_p' and
        'url_p' optional.
      (svn_client__get_youngest_common_ancestor): Don't receive unwanted outputs.

--- subversion/libsvn_client/mergeinfo.c
+++ subversion/libsvn_client/mergeinfo.c
@@ -1807,10 +1807,9 @@ svn_client_mergeinfo_log(svn_boolean_t finding_merged,
   if (!finding_merged)
     {
       svn_revnum_t target_peg_revnum;
-      const char *url;
 
       SVN_ERR(svn_client__ra_session_from_path(&target_session,
-                                               &target_peg_revnum, &url,
+                                               &target_peg_revnum, NULL,
                                                target_path_or_url, NULL,
                                                target_peg_revision,
                                                target_peg_revision,
@@ -1826,10 +1825,9 @@ svn_client_mergeinfo_log(svn_boolean_t finding_merged,
 
   {
     svn_revnum_t source_peg_revnum;
-    const char *url;
 
     SVN_ERR(svn_client__ra_session_from_path(&source_session,
-                                             &source_peg_revnum, &url,
+                                             &source_peg_revnum, NULL,
                                              source_path_or_url, NULL,
                                              source_peg_revision,
                                              source_peg_revision,
