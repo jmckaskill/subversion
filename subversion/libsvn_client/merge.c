jmckaskill/subversion

commit 109838cba21a9e38297039db8bafb3c94dc03907

Remove a hacky use of a libsvn_wc function, by reusing some existing libsvn_client code.
* subversion/include/private/svn_wc_private.h
  (svn_wc__versioned_file_modcheck): Remove.

* subversion/libsvn_wc/questions.c
  (svn_wc__versioned_file_modcheck): Remove.

* subversion/libsvn_client/client.h
  (svn_client__get_normalized_stream): New.

* subversion/libsvn_client/merge.c
  (files_same_p): Do the comparison manually, using a couple of streams, rather
    than with a libsvn_wc API.

* subversion/libsvn_client/cat.c
  (cat_local_file): Rename from this...
  (svn_client__get_normalized_stream): ...to this.
  (svn_client_cat2): Update caller.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@961254 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/merge.c
@@ -1850,12 +1850,24 @@ files_same_p(svn_boolean_t *same,
                             scratch_pool));
   if (*same)
     {
-      svn_boolean_t modified;
+      svn_stream_t *mine_stream;
+      svn_stream_t *older_stream;
+      svn_opt_revision_t working_rev;
+
+      working_rev.kind = svn_opt_revision_working;
 
       /* Compare the file content, translating 'mine' to 'normal' form. */
-      SVN_ERR(svn_wc__versioned_file_modcheck(&modified, wc_ctx, mine_abspath,
-                                              older_abspath, scratch_pool));
-      *same = !modified;
+      SVN_ERR(svn_client__get_normalized_stream(&mine_stream, wc_ctx,
+                                                mine_abspath, &working_rev,
+                                                FALSE, NULL, NULL,
+                                                scratch_pool, scratch_pool));
+
+      SVN_ERR(svn_stream_open_readonly(&older_stream, older_abspath,
+                                       scratch_pool, scratch_pool));
+
+      SVN_ERR(svn_stream_contents_same2(same, mine_stream, older_stream,
+                                        scratch_pool));
+
     }
 
   return SVN_NO_ERROR;

subversion/include/private/svn_wc_private.h
@@ -73,25 +73,6 @@ svn_wc__changelist_match(svn_wc_context_t *wc_ctx,
                          apr_hash_t *clhash,
                          apr_pool_t *scratch_pool);
 
-
-/* Set *MODIFIED_P to true if VERSIONED_FILE_ABSPATH is modified with respect
- * to BASE_FILE_ABSPATH, or false if it is not.  The comparison compensates
- * for VERSIONED_FILE_ABSPATH's eol and keyword properties, but leaves
- * BASE_FILE_ABSPATH alone (as though BASE_FILE_ABSPATH were a text-base file,
- * which it usually is, only sometimes we're calling this on incoming
- * temporary text-bases).
- *
- * If an error is returned, the effect on *MODIFIED_P is undefined.
- *
- * Use SCRATCH_POOL for temporary allocation; WC_CTX is the normal thing.
- */
-svn_error_t *
-svn_wc__versioned_file_modcheck(svn_boolean_t *modified_p,
-                                svn_wc_context_t *wc_ctx,
-                                const char *versioned_file_abspath,
-                                const char *base_file_abspath,
-                                apr_pool_t *scratch_pool);
-
 /**
  * Return a boolean answer to the question "Is @a status something that
  * should be reported?".  @a no_ignore and @a get_all are the same as

subversion/libsvn_client/cat.c
@@ -44,18 +44,16 @@
 
 /*** Code. ***/
 
-/* Helper function to handle copying a potentially translated version of
-   local file LOCAL_ABSPATH to OUTPUT.  REVISION must be one of the following:
-   BASE, COMMITTED, WORKING.  Uses SCRATCH_POOL for temporary allocations. */
-static svn_error_t *
-cat_local_file(svn_stream_t **normal_stream,
-               svn_wc_context_t *wc_ctx,
-               const char *local_abspath,
-               const svn_opt_revision_t *revision,
-               svn_cancel_func_t cancel_func,
-               void *cancel_baton,
-               apr_pool_t *result_pool,
-               apr_pool_t *scratch_pool)
+svn_error_t *
+svn_client__get_normalized_stream(svn_stream_t **normal_stream,
+                                  svn_wc_context_t *wc_ctx,
+                                  const char *local_abspath,
+                                  const svn_opt_revision_t *revision,
+                                  svn_boolean_t expand_keywords,
+                                  svn_cancel_func_t cancel_func,
+                                  void *cancel_baton,
+                                  apr_pool_t *result_pool,
+                                  apr_pool_t *scratch_pool)
 {
   apr_hash_t *kw = NULL;
   svn_subst_eol_style_t style;
@@ -165,7 +163,7 @@ cat_local_file(svn_stream_t **normal_stream,
 
   /* Wrap the output stream if translation is needed. */
   if (eol != NULL || kw != NULL)
-    input = svn_subst_stream_translated(input, eol, FALSE, kw, TRUE,
+    input = svn_subst_stream_translated(input, eol, FALSE, kw, expand_keywords,
                                         result_pool);
 
   *normal_stream = input;
@@ -212,9 +210,10 @@ svn_client_cat2(svn_stream_t *out,
       svn_stream_t *normal_stream;
 
       SVN_ERR(svn_dirent_get_absolute(&local_abspath, path_or_url, pool));
-      SVN_ERR(cat_local_file(&normal_stream, ctx->wc_ctx, local_abspath,
-                             revision, ctx->cancel_func, ctx->cancel_baton,
-                             pool, pool));
+      SVN_ERR(svn_client__get_normalized_stream(&normal_stream, ctx->wc_ctx,
+                                            local_abspath, revision, TRUE,
+                                            ctx->cancel_func, ctx->cancel_baton,
+                                            pool, pool));
 
       /* We don't promise to close output, so disown it to ensure we don't. */
       output = svn_stream_disown(output, pool);

subversion/libsvn_client/client.h
@@ -1055,6 +1055,20 @@ svn_client__ensure_revprop_table(apr_hash_t **revprop_table_out,
                                  svn_client_ctx_t *ctx,
                                  apr_pool_t *pool);
 
+/* Return a potentially translated version of local file LOCAL_ABSPATH
+   in NORMAL_STREAM.  REVISION must be one of the following: BASE, COMMITTED,
+   WORKING.  Uses SCRATCH_POOL for temporary allocations. */
+svn_error_t *
+svn_client__get_normalized_stream(svn_stream_t **normal_stream,
+                                  svn_wc_context_t *wc_ctx,
+                                  const char *local_abspath,
+                                  const svn_opt_revision_t *revision,
+                                  svn_boolean_t expand_keywords,
+                                  svn_cancel_func_t cancel_func,
+                                  void *cancel_baton,
+                                  apr_pool_t *result_pool,
+                                  apr_pool_t *scratch_pool);
+
 
 /* Return true if KIND is a revision kind that is dependent on the working
  * copy. Otherwise, return false. */

subversion/libsvn_wc/questions.c
@@ -246,26 +246,6 @@ svn_wc__internal_versioned_file_modcheck(svn_boolean_t *modified_p,
 }
 
 svn_error_t *
-svn_wc__versioned_file_modcheck(svn_boolean_t *modified_p,
-                                svn_wc_context_t *wc_ctx,
-                                const char *versioned_file_abspath,
-                                const char *base_file_abspath,
-                                apr_pool_t *scratch_pool)
-{
-  svn_stream_t *pristine_stream;
-
-  SVN_ERR_ASSERT(svn_dirent_is_absolute(base_file_abspath));
-  SVN_ERR(svn_stream_open_readonly(&pristine_stream, base_file_abspath,
-                                   scratch_pool, scratch_pool));
-
-  return svn_error_return(svn_wc__internal_versioned_file_modcheck(
-                            modified_p, wc_ctx->db, versioned_file_abspath,
-                            pristine_stream,
-                            TRUE /* compare_textbases */,
-                            scratch_pool));
-}
-
-svn_error_t *
 svn_wc__internal_text_modified_p(svn_boolean_t *modified_p,
                                  svn_wc__db_t *db,
                                  const char *local_abspath,

commit 6ccd146c74db7a7a2e3bada1d1025d3c8b278c19

Fix issue #4052, "reintegrate merge and deleted symbolic links".
Merges would erroneously flag a tree conflict for any incoming deletion
of a symlink.

* subversion/libsvn_client/merge.c
  (files_same_p): Properly compare symlinks with one another, preventing a
   spurious tree conflict on symlinks via merge_file_deleted(). This used to
   work fine in 1.6. But at some point during 1.7 development it accidentally
   stopped working. The regression was probably introduced in r961254, since
   it changed the code path used for file comparison. I haven't built an older
   client to verify this claim, though. Too bad this slipped through the test
   suite back then.

* subversion/tests/cmdline/merge_reintegrate_tests.py
  (reintegrate_symlink_deletion): Remove XFail marker.

subversion/libsvn_client/merge.c
@@ -1995,10 +1995,14 @@ files_same_p(svn_boolean_t *same,
       svn_opt_revision_t working_rev = { svn_opt_revision_working, { 0 } };
 
       /* Compare the file content, translating 'mine' to 'normal' form. */
-      SVN_ERR(svn_client__get_normalized_stream(&mine_stream, wc_ctx,
-                                                mine_abspath, &working_rev,
-                                                FALSE, TRUE, NULL, NULL,
-                                                scratch_pool, scratch_pool));
+      if (svn_prop_get_value(working_props, SVN_PROP_SPECIAL) != NULL)
+        SVN_ERR(svn_subst_read_specialfile(&mine_stream, mine_abspath,
+                                           scratch_pool, scratch_pool));
+      else
+        SVN_ERR(svn_client__get_normalized_stream(&mine_stream, wc_ctx,
+                                                  mine_abspath, &working_rev,
+                                                  FALSE, TRUE, NULL, NULL,
+                                                  scratch_pool, scratch_pool));
 
       SVN_ERR(svn_stream_open_readonly(&older_stream, older_abspath,
                                        scratch_pool, scratch_pool));

subversion/tests/cmdline/merge_reintegrate_tests.py
@@ -2544,7 +2544,6 @@ def reintegrate_replaced_source(sbox):
 
 #----------------------------------------------------------------------
 @SkipUnless(svntest.main.is_posix_os)
-@XFail()
 @Issue(4052)
 def reintegrate_symlink_deletion(sbox):
   "reintegrate symlink deletion"