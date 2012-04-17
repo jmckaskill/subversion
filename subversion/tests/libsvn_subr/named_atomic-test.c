f50a2f31c832ad1aa083fd5ac29baf359c7c46f0
philip <philip@13f79535-47bb-0310-9956-ffa450edef68>
2012-04-17 09:42:42 +0000

    * subversion/tests/libsvn_subr/named_atomic-test.c   (test_bignums, test_multiple_atomics, test_namespaces,    test_multithreaded, test_multiprocess): Add initialisation so tests     can be run individually.

--- subversion/tests/libsvn_subr/named_atomic-test.c
+++ subversion/tests/libsvn_subr/named_atomic-test.c
@@ -483,6 +483,8 @@ test_bignums(apr_pool_t *pool)
   if (!has_sufficient_privileges())
     return SVN_NO_ERROR;
 
+  SVN_ERR(init_test_shm(pool));
+
   /* Use a separate namespace for our tests isolate them from production */
   SVN_ERR(svn_atomic_namespace__create(&ns, name_namespace, pool));
 
@@ -536,6 +538,8 @@ test_multiple_atomics(apr_pool_t *pool)
   /* skip tests if the current user does not have the requried privileges */
   if (!has_sufficient_privileges())
     return SVN_NO_ERROR;
+
+  SVN_ERR(init_test_shm(pool));
 
   /* Use a separate namespace for our tests isolate them from production */
   SVN_ERR(svn_atomic_namespace__create(&ns, name_namespace, pool));
@@ -603,6 +607,8 @@ test_namespaces(apr_pool_t *pool)
   if (!has_sufficient_privileges())
     return SVN_NO_ERROR;
 
+  SVN_ERR(init_test_shm(pool));
+
   /* Use a separate namespace for our tests isolate them from production */
   SVN_ERR(svn_atomic_namespace__create(&test_namespace1, name_namespace1, pool));
   SVN_ERR(svn_atomic_namespace__create(&test_namespace1_alias, name_namespace1, pool));
@@ -645,6 +651,8 @@ test_multithreaded(apr_pool_t *pool)
   if (!has_sufficient_privileges())
     return SVN_NO_ERROR;
 
+  SVN_ERR(init_test_shm(pool));
+
   SVN_ERR(calibrate_concurrency(pool));
 
   SVN_ERR(init_concurrency_test_shm(pool, hw_thread_count));
@@ -660,6 +668,8 @@ test_multiprocess(apr_pool_t *pool)
   /* skip tests if the current user does not have the requried privileges */
   if (!has_sufficient_privileges())
     return SVN_NO_ERROR;
+
+  SVN_ERR(init_test_shm(pool));
 
   SVN_ERR(calibrate_concurrency(pool));
 
